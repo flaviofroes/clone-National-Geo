ca7d1e260a2e23ca6507adc4d87e7219bd29a939
flaviofroes <terfroesflavio@gmail.com>
2025-07-11 12:45:57 -0300

    modificações no java

--- src/script/modules/menuMobile.js
+++ src/script/modules/menuMobile.js
@@ -1,12 +1,18 @@
 export default function initMenuMobile() {
     const buttonMobile = document.querySelector('[data-menu="mobile"]');
     const ulList = document.querySelector('[data-menu="list"]');
-
+    const clickForaMobile = document.querySelector('body');
     buttonMobile.addEventListener('click', abrirMenuMobile);
 
     function abrirMenuMobile(){
         ulList.classList.toggle('ativo');
         buttonMobile.classList.toggle('ativo');
-        
+
+        clickForaMobile.addEventListener("mousedown", (event)=>{
+            if(event.target !== buttonMobile && event.target !== ulList){
+                ulList.classList.remove('ativo');
+                buttonMobile.classList.remove('ativo');
+            }
+        })
     }
 }
